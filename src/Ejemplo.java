563839ced3ea70478151601283a2ebf1e35c9593
carito <caritojac.campos@gmail.com>
2021-09-02 21:55:52 -0400

    se imprime nombre de usuario

--- src/Ejemplo.java
+++ src/Ejemplo.java
@@ -3,7 +3,14 @@ public class Ejemplo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		System.out.println("Hola nuevamente");
+		
+		String nombre = "Carolaine";
+		
+		System.out.println(nombre);
+
+		
 
 	}
 
